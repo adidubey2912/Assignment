7cc0c34b1d94c72550ac755ab6da3373cc58186b
Aditya <adi.dubey2912@gmail.com>
2022-08-17 17:29:14 +0530

    Calculate Daily Employee Wage.

--- empwage.sh
+++ empwage.sh
@@ -3,11 +3,20 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch."
 
 present=1
-attendcheck=$((RANDOM%2))
-if (($present == $attendcheck))
+attendCheck=$((RANDOM%2))
+if (($present == $attendCheck))
 then
 echo "Employee is Present."
 else
 echo "Employee is Absent."
 fi
 
+wagePerHour=20
+fullDayHour=8
+if (($present == $attendCheck))
+then
+salary=$(($wagePerHour * $fullDayHour))
+echo "Employee daily wage is $salary."
+else
+echo "Employee daily wage is 0."
+fi
