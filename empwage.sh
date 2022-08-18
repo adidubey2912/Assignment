963c30370dbfd449a2e662166809a1b34ce87b73
Aditya <adi.dubey2912@gmail.com>
2022-08-18 20:35:01 +0530

    Using switch case statement.

--- empwage.sh
+++ empwage.sh
@@ -4,29 +4,31 @@ echo "Welcome to Employee Wage Computation Program on Master Branch."
 
 present=1
 attendCheck=$((RANDOM%2))
-if (($present == $attendCheck))
-then
-echo "Employee is Present."
-else
-echo "Employee is Absent."
-fi
-
 wagePerHour=20
 fullDayHour=8
-if (($present == $attendCheck))
-then
-salary=$(($wagePerHour * $fullDayHour))
-echo "Employee daily wage is $salary."
-else
-echo "Employee daily wage is 0."
-fi
+case $attendCheck in
+$present)
+	echo "Employee is Present.";;
+*)
+	echo "Employee is Absent.";;
+esac
+
+
+case $attendCheck in
+$present)
+	salary=$(( $wagePerHour * $fullDayHour ))
+	echo "Employee daily wage is $salary.";;
+*)
+	echo "Employee daily wage is 0.";;
+esac
 
 partAttend=$((RANDOM%2))
 partTimeHour=8
-if (($present == $partAttend))
-then
-salary=$(($wagePerHour*$partTimeHour))
-echo "Part time employee daily wage is $salary."
-else
-echo "Part time employee is Absent."
-fi
+
+case $partAttend in
+$present)
+	salary=$(($wagePerHour*$partTimeHour))
+	echo "Part time employee daily wage is $salary.";;
+*)
+	echo "Part time employee is Absent."
+esac
